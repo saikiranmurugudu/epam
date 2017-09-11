bc7c06abacefc9ec6bcca1d806370cf6cb09dd8f
saikiranmurugudu <saikiranmurugudu@gmail.com>
2017-09-11 13:58:06 +0530

    Add files via upload

---
+++ students-array/student-array/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+public class Main {
+
+	public static void main(String[] args) {
+
+	    //You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+		StudentGroup stu=new StudentGroup(10);
+	    Date dd=new Date(1999,9,10);
+        Student stud=new Student(20,"abc",d,89.0);
+        stu.addFirst(s1);
+        System.out.println(stu.getStudent(0).getId());
+
+
+
+
+
+
+	}
+
+}
